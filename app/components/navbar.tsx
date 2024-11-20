70e1342117b0c5a122243755369331a0a4a3694e
HandikaDiaz <handikaalexandriadiaz@gmail.com>
2024-11-20 18:45:32 +0700

    add navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,14 +1,25 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Icon } from '@iconify/react';
+import Logo from "@/app/icon/Icon.svg";
 
 export default function Navbar() {
     return (
-        <div className="flex items-center gap-4">
-            <Icon icon="line-md:search" className="text-black w-6 h-6" />
-            <Input type="search" placeholder="City" />
-            <Button>Sign In</Button>
-            <Button>Sign Up</Button>
+        <div className="flex items-center gap-4 h-14">
+            <img src={Logo} alt="Logo" className="flex-2 w-32"/>
+            <div className="relative flex items-center flex-1">
+                <Icon icon="line-md:search" className="absolute left-2 text-black w-6 h-6" />
+                <Input
+                    type="search"
+                    placeholder="City"
+                    className="pl-10"
+                    style={{ border: 'none', backgroundColor: '#f0f0f0' }}
+                />
+            </div>
+            <div className="grid gap-4 grid-cols-2">
+                <Button>Sign In</Button>
+                <Button>Sign Up</Button>
+            </div>
         </div>
-    )
+    );
 }
